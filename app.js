af7beaaf6f5773985ddb66cbfb2c07df68a9ffef
Michael Weber <mscottweber@gmail.com>
2019-01-31 09:20:03 -0600

    Add project

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const request = require("request");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.get("/results", function(req, res) {
+  let search = "q=" + req.query.search;
+  let ingredients = "i=" + req.query.ingredients;
+  let url = `http://www.recipepuppy.com/api/?${search}&${ingredients}`;
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      let recipes = JSON.parse(body);
+      res.render("results", {recipes: recipes});
+    }
+  });
+});
+
+
+app.listen(3000, function() {
+  console.log("SERVER HAS STARTED: LOCALHOST:3000");
+});
